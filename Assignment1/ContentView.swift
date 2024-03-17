961b57292e99dff50360e2db27e03f978936ac4f
aws91031 <aws91031@gmail.com>
2024-03-16 21:23:51 -0400

    Assignment 1 Submission

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -4,114 +4,20 @@
 // Copyright © 2023 Auburn University.
 // All Rights Reserved.
 
-
 import SwiftUI
 
-struct Emoji {
-    var emoji: String
-    var counter: Int
-}
-
-var emojis = [
-    Emoji(emoji: "🤣", counter: 0),
-    Emoji(emoji: "😜", counter: 0),
-    Emoji(emoji: "😀", counter: 0),
-    Emoji(emoji: "🤩", counter: 0),
-    Emoji(emoji: "😂", counter: 0)
-]
-
-//All 5 counters increment/decrement at the same time
-/*struct ContentView: View {
+var emojis = ["🤣", "😜", "😀", "🤩", "😂"]
+              
+struct ContentView: View {
     @State var counter = 0
     var body: some View {
         NavigationView {
             List {
                 Section {
                     ForEach(0 ..< 5) {
-                        value in HStack {
-                            Text(emojis[value].emoji + "  Counter: \(counter)")
-                            Image(systemName: "plus")
-                                .imageScale(.large)
-                                .onTapGesture {
-                                    counter += 1
-                                }
-                                .buttonStyle(BorderlessButtonStyle())
-                            Image(systemName: "minus")
-                                .imageScale(.large)
-                                .onTapGesture {
-                                    counter -= 1
-                                }
-                                .buttonStyle(BorderlessButtonStyle())
+                        value in
+                            ActionItem(data : emojis[value])
                         }
-                    }
-                }
-                
-            }
-            .navigationTitle("Emoji Counter")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-    }
-}*/
-
-//Buttons do not affect the counter
-/*struct ContentView: View {
-    var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    ForEach(0 ..< 5) {
-                        value in HStack {
-                            Text(emojis[value].emoji +
-                                 "  Counter: \(emojis[value].counter)")
-                            Image(systemName: "plus")
-                                .imageScale(.large)
-                                .onTapGesture {
-                                    emojis[value].counter += 1
-                                }
-                                .buttonStyle(BorderlessButtonStyle())
-                            Image(systemName: "minus")
-                                .imageScale(.large)
-                                .onTapGesture {
-                                    emojis[value].counter -= 1
-                                }
-                                .buttonStyle(BorderlessButtonStyle())
-                        }
-                    }
-                }
-            }
-            .navigationTitle("Emoji Counter")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-    }
-}*/
-
-struct ContentView: View {
-    var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    ForEach(0 ..< 5) {
-                        value in HStack {
-                            Text(emojis[value].emoji +
-                                 "  Counter: \(emojis[value].counter)")
-                            /*Button(action: {
-                                emojis[value].counter += 1
-                            }) {
-                                Image(systemName: "plus")
-                            }.buttonStyle(BorderlessButtonStyle())*/
-                            Button {
-                                emojis[value].counter += 1
-                            } label: {
-                                Image(systemName: "plus")
-                            }.buttonStyle(BorderlessButtonStyle())
-                            Button(action: {
-                                emojis[value].counter -= 1
-                            }) {
-                                Image(systemName: "minus")
-                            }.buttonStyle(BorderlessButtonStyle())
-
-                        }
-                    }
                 }
             }
             .navigationTitle("Emoji Counter")
@@ -119,6 +25,33 @@ struct ContentView: View {
         }
     }
 }
+
+struct ActionItem: View {
+    
+    @State var counter = 0
+    var data : String
+    
+    var body: some View {
+        HStack {
+            Text(data +  " Counter: ")
+            Text("\(counter)")
+            Spacer()
+            Image(systemName: "plus")
+                .imageScale(.large)
+                .onTapGesture {
+                    counter += 1
+                }
+            Image(systemName: "minus")
+                .imageScale(.large)
+                .onTapGesture {
+                    counter -= 1
+                }
+        }
+        .padding()
+        .foregroundColor(.accentColor)
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -127,149 +60,3 @@ struct ContentView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
-
-/*struct ContentView: View {
-    var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    ActionItem()
-                    ActionItem2()
-                    ActionItem3()
-                    ActionItem4()
-                    ActionItem5()
-                }
-            }
-            .listStyle(.grouped)
-            .navigationTitle("Emoji Counter")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-    }
-}*/
-
-
-
-/*struct ActionItem: View {
-    
-    @State var counter = 0
-    
-    var body: some View {
-        HStack {
-            Text("🤣 Counter: ")
-            Text("\(counter)")
-            Spacer()
-            Image(systemName: "plus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter += 1
-                }
-            Image(systemName: "minus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter -= 1
-                }
-        }
-        .padding()
-        .foregroundColor(.accentColor)
-    }
-}
-
-struct ActionItem2: View {
-    
-    @State var counter = 0
-    
-    var body: some View {
-        HStack {
-            Text("😜 Counter: ")
-            Text("\(counter)")
-            Spacer()
-            Image(systemName: "plus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter += 1
-                }
-            Image(systemName: "minus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter -= 1
-                }
-        }
-        .padding()
-        .foregroundColor(.accentColor)
-    }
-}
-
-struct ActionItem3: View {
-    
-    @State var counter = 0
-    
-    var body: some View {
-        HStack {
-            Text("😀 Counter: ")
-            Text("\(counter)")
-            Spacer()
-            Image(systemName: "plus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter += 1
-                }
-            Image(systemName: "minus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter -= 1
-                }
-        }
-        .padding()
-        .foregroundColor(.accentColor)
-    }
-}
-
-struct ActionItem4: View {
-    
-    @State var counter = 0
-    
-    var body: some View {
-        HStack {
-            Text("🤩 Counter: ")
-            Text("\(counter)")
-            Spacer()
-            Image(systemName: "plus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter += 1
-                }
-            Image(systemName: "minus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter -= 1
-                }
-        }
-        .padding()
-        .foregroundColor(.accentColor)
-    }
-}
-
-struct ActionItem5: View {
-    
-    @State var counter = 0
-    
-    var body: some View {
-        HStack {
-            Text("😂 Counter: ")
-            Text("\(counter)")
-            Spacer()
-            Image(systemName: "plus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter += 1
-                }
-            Image(systemName: "minus")
-                .imageScale(.large)
-                .onTapGesture {
-                    counter -= 1
-                }
-        }
-        .padding()
-        .foregroundColor(.accentColor)
-    }
-}*/
